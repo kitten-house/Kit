d4677b8126e96940aaa58fc204e1d1d7ecfd95d7
andreichupin <andreichupin@binance.com>
2023-10-14 23:23:26 +0300

    Sync

--- auth/src/main/java/com/kit/backend/auth/controller/AuthController.java
+++ auth/src/main/java/com/kit/backend/auth/controller/AuthController.java
@@ -37,7 +37,7 @@ public class AuthController {
         this.authServiceImp = authServiceImp;
     }
 
-    @PostMapping("/login") // https://www.googleapis.com/oauth2/v1/userinfo?alt=json
+    @PostMapping("/login")
     public ResponseEntity<AuthResponseBody> login(
         @RequestBody LoginRequestBody body
     ) {
@@ -94,58 +94,33 @@ public class AuthController {
             .build();
     }
 
-    String token = "eyJhbGciOiJSUzI1NiIsImtpZCI6ImQzN2FhNTA0MzgxMjkzN2ZlNDM5NjBjYTNjZjBlMjI4NGI2ZmMzNGQiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJuYmYiOjE2NzM3ODY3ODgsImF1ZCI6Ijg5NTA2MTIxNzM0MC12Y2tqMmdiaGppZWtwaGtnNWxnM2Y2ZWFmMWU1YzB0di5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsInN1YiI6IjExNTYzNjA5NTE5MDA2ODY3MTI1MCIsImVtYWlsIjoiYW5kcmV3Y2h1cGluOTZAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImF6cCI6Ijg5NTA2MTIxNzM0MC12Y2tqMmdiaGppZWtwaGtnNWxnM2Y2ZWFmMWU1YzB0di5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsIm5hbWUiOiJBbmRyZXcgQ2h1cGluIiwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS9hL0FFZEZUcDRqbTJVWU1rWXFSV01EUkpkOXB3QTltR21xNG5PTnV6WlhweDB3PXM5Ni1jIiwiZ2l2ZW5fbmFtZSI6IkFuZHJldyIsImZhbWlseV9uYW1lIjoiQ2h1cGluIiwiaWF0IjoxNjczNzg3MDg4LCJleHAiOjE2NzM3OTA2ODgsImp0aSI6ImU0ZTZlZjFiZmUxMjYzOTA5OWVlMTRhNDcwNDM1MzdmNzdjZmM2YTQifQ.ZuBqKt7ontpAcVTZga036l3MI5s6WHg7Fbk03XSKRm2TRuCoei0x42fQsHJ7V1dKDHq2O3zzHljEE7qo6ujVpnPASrqftO90hcY9-48vazdc9YPD2FTDv0VSN0fP8nFykt8N0l-HehguBUr_sdIjF8VBDGfnTH2V5q3ffjEMPbnmfq960oHdGDbXXyzK88jYAnIGGB5Trl2Kl8qn3Tu1PlyigeKpa5kk01TYYQcXM4sHAcK2Cp0GHHaNKJZ3EeL2nyleFXCahrgW-B5eGp1j4PVNveLx-ZHa1QT3EGOrKBWJgzol10FWjbMlIHYMvRo3qJGihZktuumY5pXkwDw8jg";
-
     @PostMapping("/me")
     public ResponseEntity<User> getUser(
-//        @Nullable
-//        @CookieValue(name = "refreshToken", required = false)
-//        String refreshToken,
-//
-//        @Nullable
-//        @RequestHeader(name = "Authorization", required = false)
-//        String authorization
+            @Nullable
+            @CookieValue(name = "refreshToken", required = false)
+            String refreshToken,
+
+            @Nullable
+            @RequestHeader(name = "Authorization", required = false)
+            String authorization
         )
     {
-        System.out.println("BLA1");
-//        Optional<String> token = validateToken(refreshToken, authorization);
-//
-//        if (token.isEmpty()) {
-//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-//        }
-//
-//        Optional<User> optional = authServiceImp.getUserByToken(token.get());
-//
-//        if (optional.isEmpty()) {
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-//        }
+        Optional<String> token = validateToken(refreshToken, authorization);
 
-        RestTemplate template = new RestTemplate();
-        String url = "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=" + token;
-
-        try {
-            Logger.getGlobal().log(Level.INFO,"BLA1");
-            HttpEntity<String> entity = new HttpEntity<>(new HttpHeaders());
-            ResponseEntity<GoogleAccount> result = template.exchange(url, HttpMethod.GET, entity, GoogleAccount.class);
-            Logger.getGlobal().log(Level.INFO,"BLA2");
-            GoogleAccount json = result.getBody();
-            Logger.getGlobal().log(Level.INFO, "BLABLA " + json + " dsa " + result.getStatusCode());
-        } catch (Throwable e) {
-            e.printStackTrace();
+        if (token.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
 
-        User user = new User();
-        user.setId(1);
-        user.setName("Andrew Chupin");
-        user.setAvatar("ava");
-        user.setGoogleId("2321");
+        Optional<User> optional = authServiceImp.getUserByToken(token.get());
+
+        if (optional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
 
         HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
-        return ResponseEntity.ok().headers(responseHeaders).body(user);
+        return ResponseEntity.ok().headers(responseHeaders).body(optional.get());
     }
-
-
 
     private Optional<String> validateToken(String refreshToken, String authorization) {
         String token;
@@ -160,6 +135,5 @@ public class AuthController {
 
         return Optional.of(token);
     }
-
 }
 
